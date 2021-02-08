d53bcadae7b7e5cc4e9f094de598c5d0abea85f7
mayr0y <alexlzrw@gmail.com>
2021-02-08 13:20:34 +0300

    added ChatHub.cs

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRWeb.Hubs {
+    public class ChatHub : Hub {
+        public async Task NewMessage(long username, string message){
+            await Clients.All.SendAsync("messageReceived", username, message);
+        }
+    }
+}
